5f5797cd70e0ae5510b6e36c0f3b4a61075c4c8e
mesut2ooo <124920843+mesut2ooo@users.noreply.github.com>
2024-02-12 15:11:31 +0330

    Create 02.Operators.py

---
+++ 1.Basics/02.Operators.py
@@ -0,0 +1,21 @@
+################################
+#   Comparison:
+################################
+# You can use comparison operator on numbers or strings
+# We have 6 operators:
+#  <, <=, >, >=, ==, !=
+
+# They all return a boolean
+
+################################
+#   Logical:
+################################
+# To check multiple conditions
+# and     # if both of them are true
+# or      # At least one of them is true
+# not     # Unary operator , reverses the value
+
+a < 10 or b > 15
+
+# Precedence
+#     not -> and -> or
